2ae18733647603c9d8499069c353128a8c11311c
B3ella <goliveirapk@gmail.com>
2026-06-07 20:11:44 -0300

    mock backend

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express'),
+app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/minhas_tarefas', 
+   (req, res) => res.send('minhas tarefas')
+)
+app.post('/concluir_tarefa', 
+   (req, res) => res.send('tarefa concluida')
+)
+
+app.get('/tarefas_disponiveis', 
+   (req, res) => res.send('tarefas disponiveis')
+)
+app.post('/aceitar_tarefa', 
+   (req, res) => res.send('tarefa aceita')
+)
+
+app.get('/recompensas_disponiveis', 
+   (req, res) => res.send('recompensas disponiveis')
+)
+app.post('/resgatar_recompensa', 
+   (req, res) => res.send('recompensa resgatada')
+)
+
+const port = 8080;
+app.listen(port, 
+   () => console.log(`⚡️[bootup]: Server is running at port: ${port}`)
+);
+
